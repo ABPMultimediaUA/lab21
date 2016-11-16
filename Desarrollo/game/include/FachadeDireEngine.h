c143bc119192dd159e87e59886066f9ade5a04b6
Ruben MM <ruben2m@gmail.com>
2016-11-16 10:16:19 +0100

    Fachada Irrlicht. Carga de un mesh .obj con textura y movimiento

--- Desarrollo/game/include/FachadeDireEngine.h
+++ Desarrollo/game/include/FachadeDireEngine.h
@@ -1,6 +1,22 @@
 #ifndef FACHADEDIREENGINE_H
 #define FACHADEDIREENGINE_H
 
+#include <irrlicht.h>
+#include <string>
+#include <AppReceiver.h>
+
+
+using namespace std;
+
+// Aun estando los namespace sigo definiendo las propiedades
+// de los objetos con irr::core... para que c:b autocomplete bien
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
 namespace fde
 {
     ///////////////////////////////////////////////
@@ -27,6 +43,47 @@ namespace fde
     };
     typedef vec3<float> vec3f;
     typedef vec3<int> vec3i;
+
+
+    ///////////////////////////////////////////////
+    // Node
+    // ====
+    //
+    ///////////////////////////////////////////////
+    class Node
+    {
+    public:
+        Node(ISceneNode* n);
+        void move(vec3f v);
+    private:
+        irr::scene::ISceneNode* m_node;
+    };
+
+
+
+    ///////////////////////////////////////////////
+    // Graphics
+    // ========
+    //
+    ///////////////////////////////////////////////
+    class Graphics
+    {
+    public:
+        void init(AppReceiver* ar = 0);
+        void release();
+        void close();
+        bool isRunning();
+        void draw();
+        Node* createNode(std::string meshName);
+        bool isWindowActive();
+        void yield();
+
+    private:
+        irr::IrrlichtDevice*            m_device;
+        irr::video::IVideoDriver*       m_driver;
+        irr::scene::ISceneManager*      m_smgr;
+        irr::gui::IGUIEnvironment*      m_guienv;
+    };
 }
 
 #endif // FACHADEDIREENGINE_H
--- Desarrollo/game/src/main.cpp
+++ Desarrollo/game/src/main.cpp
@@ -1,132 +1,60 @@
 #include <iostream>
-#include <irrlicht.h>
-
 #include <Box2D/Box2D.h>
+#include <FachadeDireEngine.h>
+#include <AppReceiver.h>
 
-
-using namespace std;
-
-using namespace irr;
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
-
-#ifdef _IRR_WINDOWS_
-#pragma comment(lib, "Irrlicht.lib")
-#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
-#endif
 
 int main()
 {
-    /* Box2D */
+    // Box2D
     b2BodyDef groundBodyDef;
     groundBodyDef.position.Set(0.0f, -10.0f);
 
 
 
-    /* Illricht */
-	IrrlichtDevice *device = createDevice( video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16,
-			false, false, false, 0);
+    // Illricht
+    AppReceiver* appReceiver = new AppReceiver();
+	fde::Graphics graphics;
+	graphics.init(appReceiver);
 
-	if (!device)
-		return 1;
+	fde::Node* mono = graphics.createNode("mono");
 
-	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
 
-	/*
-	Get a pointer to the VideoDriver, the SceneManager and the graphical
-	user interface environment, so that we do not always have to write
-	device->getVideoDriver(), device->getSceneManager(), or
-	device->getGUIEnvironment().
-	*/
-	IVideoDriver* driver = device->getVideoDriver();
-	ISceneManager* smgr = device->getSceneManager();
-	IGUIEnvironment* guienv = device->getGUIEnvironment();
-
-	/*
-	We add a hello world label to the window, using the GUI environment.
-	The text is placed at the position (10,10) as top left corner and
-	(260,22) as lower right corner.
-	*/
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-		rect<s32>(10,10,260,22), true);
-
-	/*
-	To show something interesting, we load a Quake 2 model and display it.
-	We only have to get the Mesh from the Scene Manager with getMesh() and add
-	a SceneNode to display the mesh with addAnimatedMeshSceneNode(). We
-	check the return value of getMesh() to become aware of loading problems
-	and other errors.
-
-	Instead of writing the filename sydney.md2, it would also be possible
-	to load a Maya object file (.obj), a complete Quake3 map (.bsp) or any
-	other supported file format. By the way, that cool Quake 2 model
-	called sydney was modelled by Brian Collins.
-	*/
-	IAnimatedMesh* mesh = smgr->getMesh("sydney.md2");
-	if (!mesh)
+	while(graphics.isRunning())
 	{
-		device->drop();
-		return 1;
-	}
-	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
+	    if (graphics.isWindowActive())
+        {
+            fde::vec3f pos(0.0f);
 
-	/*
-	To let the mesh look a little bit nicer, we change its material. We
-	disable lighting because we do not have a dynamic light in here, and
-	the mesh would be totally black otherwise. Then we set the frame loop,
-	such that the predefined STAND animation is used. And last, we apply a
-	texture to the mesh. Without it the mesh would be drawn using only a
-	color.
-	*/
-	if (node)
-	{
-		node->setMaterialFlag(EMF_LIGHTING, false);
-		node->setMD2Animation(scene::EMAT_STAND);
-		node->setMaterialTexture( 0, driver->getTexture("sydney.bmp") );
+            if(appReceiver->isKeyDown(KEY_ESCAPE))
+            {
+                graphics.close();
+                return 0;
+            }
+            else
+            {
+                if(appReceiver->isKeyDown(KEY_RIGHT))
+                    pos.x += 0.02;
+                if(appReceiver->isKeyDown(KEY_LEFT))
+                    pos.x -= 0.02;
+                if(appReceiver->isKeyDown(KEY_UP))
+                    pos.y += 0.02;
+                if(appReceiver->isKeyDown(KEY_DOWN))
+                    pos.y -= 0.02;
+            }
+
+            mono->move(pos);
+
+            graphics.draw();
+        }
+        else
+        {
+            graphics.yield();
+        }
 	}
 
-	/*
-	To look at the mesh, we place a camera into 3d space at the position
-	(0, 30, -40). The camera looks from there to (0,5,0), which is
-	approximately the place where our md2 model is.
-	*/
-	smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
-
-	/*
-	Ok, now we have set up the scene, lets draw everything: We run the
-	device in a while() loop, until the device does not want to run any
-	more. This would be when the user closes the window or presses ALT+F4
-	(or whatever keycode closes a window).
-	*/
-	while(device->run())
-	{
-		/*
-		Anything can be drawn between a beginScene() and an endScene()
-		call. The beginScene() call clears the screen with a color and
-		the depth buffer, if desired. Then we let the Scene Manager and
-		the GUI Environment draw their content. With the endScene()
-		call everything is presented on the screen.
-		*/
-		driver->beginScene(true, true, SColor(255,100,101,140));
-
-		smgr->drawAll();
-		guienv->drawAll();
-
-		driver->endScene();
-	}
-
-	/*
-	After we are done with the render loop, we have to delete the Irrlicht
-	Device created before with createDevice(). In the Irrlicht Engine, you
-	have to delete all objects you created with a method or function which
-	starts with 'create'. The object is simply deleted by calling ->drop().
-	See the documentation at irr::IReferenceCounted::drop() for more
-	information.
-	*/
-	device->drop();
 
 	return 0;
 }
+
+
