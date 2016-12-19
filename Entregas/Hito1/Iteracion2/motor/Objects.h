1a954fadc906f63952469b2e5d39edc0e354ac11
Ruben MM <ruben2m@gmail.com>
2016-12-19 20:34:56 +0100

    Organizar repositorio como ha indicado Carlos

---
+++ Entregas/Hito1/Iteracion2/motor/Objects.h
@@ -0,0 +1,143 @@
+
+/***************************************************************************************
+
+    CarGL (Objects.h)
+
+    Grado en Ingeniería Multimedia.
+    Práctica de Gráficos por Computador.
+    OpenGL con Shaders.
+  --------------------------------------------------------------------------------------
+
+    Noviembre 2012 - Septiembre 2016 (C) Juan Antonio Puchol García (puchol@dccia.ua.es)
+
+****************************************************************************************/
+
+//---------------------------------------------------------------------------
+#ifndef ObjectsH
+#define ObjectsH
+//---------------------------------------------------------------------------
+
+#define GLEW_STATIC
+
+#include "Shader.h"
+#include "Program.h"
+
+#include <GL/glui.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Identificadores internos de los objetos
+#define CUBE_ID          7
+
+
+// Datos del formato 3DS (x, y, z, A, B, C, u, v)
+#define POSITION_COMPONENT_COUNT    3
+#define NORMAL_COMPONENT_COUNT      3
+#define UV_COMPONENT_COUNT          2
+// Cálculo del stride (3+3+2)*4 = 8*4 = 32
+#define STRIDE                      32
+
+// Nombre de los attributes
+#define A_POSITION  "a_Position"
+#define A_NORMAL    "a_Normal"
+
+// Nombre de los uniforms
+#define U_PROJECTIONMATRIX      "u_ProjectionMatrix"
+#define U_MVMATRIX              "u_MVMatrix"
+#define U_VMATRIX               "u_VMatrix"
+#define U_COLOR                 "u_Color"
+#define U_LUZ0                  "u_Luz0"
+
+//************************************************************** Clase TPrimtiva
+
+class TPrimitiva
+{
+public: // Atributos de la clase
+		int ID;				    // DisplayLists del objeto
+		int tipo;               // Tipo de Objeto
+		float tx,ty,tz; 	    // Posición del objeto
+		float sx,sy,sz; 	    // Escalado del objeto
+		float rx,ry,rz;     	// Rotación del objeto
+		float colores[4];       // Color RGB y canal Alfa
+
+		float   *modelo0;        // modelo a representar
+		int     num_vertices0;   // número de vértices
+
+public: // Métodos
+ 		TPrimitiva(int DL, int tipo);
+        void __fastcall Render(int seleccion, bool reflejo=false);
+};
+
+//************************************************************** Clase TEscena
+
+class TEscena
+{
+public: // Atributos de la clase
+		int   	seleccion;   	// Objeto seleccionado, 0=ninguno
+        int		num_objects;    // Número de objetos
+
+        TPrimitiva  *objects[100];
+
+        // Handles de los attributes y uniforms
+        int aPositionLocation;
+        int aNormalLocation;
+        int uProjectionMatrixLocation;
+        int uMVMatrixLocation;
+		int uVMatrixLocation;
+		int uColorLocation;
+		int uLuz0Location;
+
+		glm::mat4 projectionMatrix; // Almacena la matriz de proyección
+        glm::mat4 viewMatrix;       // Almacena la matriz de la vista (cámara)
+
+		Program  *shaderProgram;    // Almacena el programa de OpenGL (ShaderProgram)
+
+        // Vectores de luces y materiales
+        GLfloat light0_ambient[4];
+        GLfloat light0_diffuse[4];
+        GLfloat light0_specular[4];
+        GLfloat light0_position[4];
+
+        GLfloat light1_ambient[4];
+        GLfloat light1_diffuse[4];
+        GLfloat light1_specular[4];
+        GLfloat light1_position[4];
+
+        GLfloat mat_ambient[4];
+        GLfloat mat_diffuse[4];
+        GLfloat mat_specular[4];
+        GLfloat mat_shininess[1];
+
+        float   xy_aspect;
+        int     last_x, last_y;
+
+        // live variables usadas por GLUI en TGui
+        int     wireframe;
+        int     z_buffer;
+        int     culling;
+
+        GLfloat view_position[3];
+        GLfloat view_rotate[16];
+        float   scale;
+
+public: // Métodos
+		TEscena();
+
+        void __fastcall InitGL();
+		void __fastcall Render();
+		void __fastcall RenderObjects(bool reflejo=false);
+
+		void __fastcall AddObject(TPrimitiva *object);
+
+		void __fastcall Pick3D(int mouse_x, int mouse_y);
+
+		TPrimitiva* __fastcall getObject(int i);
+
+};
+
+//************************************************************** Variables de clase
+
+extern TEscena  escena;
+
+#endif
