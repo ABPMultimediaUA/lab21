63b840b2f753c4b5a4b2cfb1c613dff0e2b4afc6
edu94against <edu.music.1994@gmail.com>
2017-03-30 09:36:55 +0200

    Limpieza de main. Nota: al disparar en online, al player que no ha disparado le deja de funcionar el juego

--- Desarrollo/game/src/main.cpp
+++ Desarrollo/game/src/main.cpp
@@ -1,20 +1,10 @@
 #include "Game.h"
 #include "GSMainMenu.h"
-#include "NetGame.h"
-#include "GSIngame.h"
-//#include "Hud.h"
 
 int main()
 {
     Game::getInstance()->ChangeState(GSMainMenu::getInstance());
     GEInstance->init();
     Game::getInstance()->Run();
-    //NetInstance->close();
-
 	return 0;
-
-	/******************/
-	//Hud* hud = new Hud();
-	//hud->draw();
-	/*****************/
 }
