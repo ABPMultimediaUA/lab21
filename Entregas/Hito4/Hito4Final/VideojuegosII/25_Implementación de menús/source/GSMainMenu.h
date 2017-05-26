608ad30488fa3556553f28cf0fb586dd3d6589ad
edu94against <edu.music.1994@gmail.com>
2017-05-26 17:55:18 +0200

    Añadiendo cosas entrega Hito4

---
+++ Entregas/Hito4/Hito4Final/VideojuegosII/25_Implementación de menús/source/GSMainMenu.h
@@ -0,0 +1,67 @@
+#ifndef GSMAINMENU_H
+#define GSMAINMENU_H
+#include <sstream>
+
+#include "GState.h"
+
+namespace dwe
+{
+    class Button;
+    class Background;
+    class Sprite;
+    class Slider;
+}
+
+class GSMainMenu : public GState{
+public:
+    GSMainMenu();
+    ~GSMainMenu();
+    void HandleEvents();
+    void Update();
+    void Render();
+    void SetPage(int n);
+    void UpdateLobbys();
+    void UpdateServers();
+    static GSMainMenu* getInstance();
+
+private:
+    int page;
+    int mousePosX, mousePosY;
+    bool menuInfo; // Mostrar info de los menus
+    bool m_clickPermission;
+    bool enterNet;
+    bool serverSelection; // Servidor elegido
+    bool serverInfo;
+    bool lobbySelection; // Lobby elegida
+    bool updatedLobbys;
+
+    std::vector<std::string>* gamesIP;
+    std::vector<std::string>* servers;
+    std::string type;
+    std::string ip;
+    std::string lobby;
+
+    /**/
+    dwe::Background *menuBackground;
+
+    dwe::Sprite *mainMenuDecoration;
+    dwe::Sprite *optionsDecoration;
+
+    dwe::Slider *volumeSlider;
+
+    dwe::Button *playAloneButton;
+    dwe::Button *playOnlineButton;
+    dwe::Button *creditsButton;
+    dwe::Button *optionsButton;
+    dwe::Button *exitButton;
+    dwe::Button *backButton;
+    dwe::Button *createLobbyButton;
+
+    dwe::Button *auxButton; // No hacer delete, se borra al borrar los vectores de abajo
+
+    std::vector<dwe::Button>* serversButtons; // Falta delete
+    std::vector<dwe::Button>* lobbysButtons; // Falta delete
+    /**/
+};
+
+#endif // GSMAINMENU_H
---
+++ Entregas/Hito4/Hito4Final/VideojuegosII/25_Implementación de menús/source/GSPause.h
@@ -0,0 +1,47 @@
+#ifndef GSPAUSE_H
+#define GSPAUSE_H
+
+#include "GState.h"
+
+namespace dwe
+{
+    class Button;
+    class Background;
+    class Sprite;
+    class Slider;
+}
+
+class GSPause : public GState{
+public:
+    GSPause();
+    ~GSPause();
+    void HandleEvents();
+    void Update();
+    void Render();
+    void SetPage(int n);
+    static GSPause* getInstance();
+    void resumeGame();
+    void backToMainMenu();
+
+private:
+    int page;
+    int mousePosX, mousePosY;
+    bool m;
+    bool m_pausePermission; // Booleano para controlar el boton pausa
+    bool m_clickPermission; // Control del click
+
+    /****/
+    dwe::Background *menuPausaFondo;
+
+    dwe::Sprite *pauseDecoration;
+    dwe::Sprite *optionsDecoration;
+
+    dwe::Slider *volumeSlider;
+
+    dwe::Button *resumeGameButton;
+    dwe::Button *helpOptionsButton;
+    dwe::Button *exitToMainMenuButton;
+    dwe::Button *backButton;
+};
+
+#endif // GSPAUSE_H
